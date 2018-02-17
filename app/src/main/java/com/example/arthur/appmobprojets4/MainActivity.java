9a7322e268f45173a3526f3754493ceb7d9caa18
ACayet <arthurcayet@hotmail.fr>
2018-02-17 02:38:52 +0100

    Google Map fonctionnel

--- app/src/main/java/com/example/arthur/appmobprojets4/MainActivity.java
+++ app/src/main/java/com/example/arthur/appmobprojets4/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.arthur.appmobprojets4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button button = (Button) findViewById(R.id.buttonMap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickAllerEcranMap(view);
+            }
+        });
+
     }
 
+    public void onClickAllerEcranMap(View view) {
+        Intent goMap = new Intent(this, MapsActivity.class);
+        startActivity(goMap);
+    }
 }
